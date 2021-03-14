9902401435cf6a813907c493c6c07e7f24ca54b0
gitakartika25 <teresagita25@gmail.com>
2021-03-14 11:52:41 +0700

    Menambahkan file constants.dart untuk menentukan nilai suatu objek

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(
+    0xFF535353); // mendefinisikan nilai TextColor yang akan dipakai pada setiap objek text
+const kTextLightColor = Color(
+    0xFFACACAC); // mendefinisikan nilai TextLightColor yang akan dipakai pada setiap objek text
+
+const kDefaultPaddin =
+    20.0; // mendefinisikan nilai DefaultPaddin yang akan dipakai untuk mengatur padding

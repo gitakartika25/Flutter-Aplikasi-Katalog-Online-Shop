e9a73e3885c16dd5508c98e5dbf1ffc6fad1e4d5
gitakartika25 <teresagita25@gmail.com>
2021-03-15 05:56:21 +0700

    Menambahkan class item_card.dart

---
+++ lib/screens/components/Item_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:kuis1/constants.dart';
+import 'package:kuis1/models/Product.dart';
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final Function press;
+  const ItemCard({
+    Key key,
+    this.product,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(kDefaultPaddin),
+            decoration: BoxDecoration(
+              color: product.color,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Image.asset(product.image),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
+          child: Text(
+            product.title,
+            style: TextStyle(color: kTextLightColor),
+          ),
+        ),
+        Text(
+          "\$${product.price}",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}

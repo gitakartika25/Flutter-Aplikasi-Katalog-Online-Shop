a8e0c0cbef6a0647ca9ebb53ad8ae6051711772a
gitakartika25 <teresagita25@gmail.com>
2021-03-14 13:44:32 +0700

    Menambahkan appBar pada class home_screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets\icons\back.svg"),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}

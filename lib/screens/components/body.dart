92b9809768e189a59d748c3e03d240d3cb1ed97a
gitakartika25 <teresagita25@gmail.com>
2021-03-14 23:31:48 +0700

    Menambahkan Column berupa image, title, price

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kuis1/constants.dart';
 import 'categories.dart';
+import 'package:kuis1/models/Product.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -19,6 +20,31 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(kDefaultPaddin),
+              height: 180,
+              width: 160,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Image.asset(products[0].image),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin / 4),
+              child: Text(
+                products[0].title,
+                style: TextStyle(color: kTextLightColor),
+              ),
+            ),
+            Text(
+              "Rp35000",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ],
     );
   }
